0bedf228290cc9d985bb2d11be1dd3e644a973b4
Nathan04Scott <natsco9120@students.ecpi.edu>
2023-12-12 10:36:43 -0500

    1.3 GP has been completed.

---
+++ first_application/routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var places = [
+    { name: 'Parent House', rating: 6 },
+    { name: 'GrandParent House', rating: 9},
+    { name: 'Church', rating: 7},
+    { name: 'Comic Book Store', rating: 8},
+    {name: 'ECPI', rating: 7}
+  ];
+
+  // create a name
+  var myname = "Nathan Scott"
+
+  res.render('index', { 
+    title: 'Favorite Places',
+  places: places,
+  myname: myname
+  });
+});
+
+module.exports = router;
